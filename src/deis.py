0718a3e8c5638c910244691b27ed60e81575d0a0
Raphael MESSNER <raphael.messner@gmail.com>
2014-11-23 12:12:52 +0100

    Async request to REST API

--- src/deis.py
+++ src/deis.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import thread
 
 deis_controller = "http://deis.23.251.142.211.xip.io"
 
@@ -44,18 +45,21 @@ def deis_create_app(token, name):
     return r.json()
 
 def deis_create_build(token, app, image): 
+    thread.start_new_thread( async_deis_create_build, (token, app, image, ) )
+    return deis_list_builds(token, app)
+
+def async_deis_create_build(token, app, image):
     url = deis_controller + "/v1/apps/" + app + "/builds"
     data = { "image": image }
     headers = {'Content-type': 'application/json', 'Authorization': 'token '+ token }
-    print "Request to deis :" + url
-    print "==> " + json.dumps(data)
     r = requests.post(url, data=json.dumps(data), headers=headers)
-    print "<== " + json.dumps(r.json())
-    return r.json()
 
 def deis_scale(token, app, count):     
+    thread.start_new_thread( async_deis_scale, (token, app, count, ) )
+    return deis_list_containers(token,app)
+
+def async_deis_scale(token, app, count):     
     url = deis_controller + "/v1/apps/" + app + "/scale"
     data = { "cmd": count }
     headers = {'Content-type': 'application/json', 'Authorization': 'token '+ token }
     r = requests.post(url, data=json.dumps(data), headers=headers)
-    return deis_list_containers(token,app)
